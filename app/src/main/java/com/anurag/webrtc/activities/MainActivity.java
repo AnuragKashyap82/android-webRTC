5ab83d97e7b411648d754d365ca23ec91ad3bbce
Anurag Kashyap <anuragkashyap815353@gmail.com>
2023-08-16 14:25:07 +0530

    android-webRTC

---
+++ app/src/main/java/com/anurag/webrtc/activities/MainActivity.java
@@ -0,0 +1,128 @@
+package com.anurag.webrtc.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.anurag.webrtc.databinding.ActivityMainBinding;
+import com.anurag.webrtc.models.User;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    long coins = 0;
+    String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+    private int requestCode = 1;
+    User user;
+//    ProgressDialog progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        binding.progressBarCyclic.setVisibility(View.VISIBLE);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        database.getReference().child("profiles")
+                .child(currentUser.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        binding.progressBarCyclic.setVisibility(View.GONE);
+                        user = snapshot.getValue(User.class);
+                        coins = user.getCoins();
+
+                        binding.coins.setText("You have: " + coins);
+
+                        Glide.with(MainActivity.this)
+                                .load(user.getProfile())
+                                .into(binding.profilePicture);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPermissionsGranted()) {
+                    if (coins > 5) {
+                        coins = coins - 5;
+                        database.getReference().child("profiles")
+                                .child(currentUser.getUid())
+                                .child("coins")
+                                .setValue(coins);
+                        Intent intent = new Intent(MainActivity.this, ConnectingActivity.class);
+                        intent.putExtra("profile", user.getProfile());
+                        startActivity(intent);
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insufficient Coins", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    askPermissions();
+                }
+            }
+        });
+
+        binding.rewardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RewardActivity.class));
+            }
+        });
+
+
+    }
+
+    void askPermissions() {
+        ActivityCompat.requestPermissions(this, permissions, requestCode);
+    }
+
+    private boolean isPermissionsGranted() {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+
+        return true;
+    }
+
+}
